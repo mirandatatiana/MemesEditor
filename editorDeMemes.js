3c40fc4e977e00c91c4f7a286ea0d3f3495a6923
Candela Olha <candelaolha@Candela.local>
2021-08-08 19:56:39 -0300

    activo el boton para ver el aside de texto

---
+++ editorDeMemes.js
@@ -0,0 +1,6 @@
+const textEditorButton = document.getElementById("text-button");
+const textEditorAside = document.querySelector(".text-editor");
+
+textEditorButton.onclick = () => {
+    textEditorAside.style.display = "block";
+}
