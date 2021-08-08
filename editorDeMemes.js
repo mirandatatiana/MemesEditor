9d00e57f6c04d426f0a5632f61f8037f7722f696
Candela Olha <candelaolha@Candela.local>
2021-08-08 20:45:00 -0300

    agrego funciones que modifican el texto del meme

--- editorDeMemes.js
+++ editorDeMemes.js
@@ -1,6 +1,24 @@
 const textEditorButton = document.getElementById("text-button");
 const textEditorAside = document.querySelector(".text-editor");
+const memeTopText = document.querySelector(".meme-top-text");
+const memeTopTextEditor = document.querySelector("#top-text-editor");
+const memeBottomText = document.querySelector(".meme-bottom-text");
+const memeBottomTextEditor = document.querySelector("#bottom-text-editor");
+
+// Boton para que se vea el aside de texto
 
 textEditorButton.onclick = () => {
     textEditorAside.style.display = "block";
+}
+
+// Input que modifica el top text del meme
+
+memeTopTextEditor.oninput = () => {
+    memeTopText.textContent = memeTopTextEditor.value;
+}
+
+// Input que modifica el bottom text del meme
+
+memeBottomTextEditor.oninput = () => {
+    memeBottomText.textContent = memeBottomTextEditor.value;
 }
