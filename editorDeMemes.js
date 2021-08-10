3d4fde5f63052bb46c048d0b7f7aa3b6b6a9db81
Candela Olha <candelaolha@Candela.local>
2021-08-10 16:51:15 -0300

    agrego funcion para cambiar la tipografia del meme

--- editorDeMemes.js
+++ editorDeMemes.js
@@ -6,6 +6,15 @@ const topTextCheckbox = document.querySelector("#no-top-text");
 const memeBottomText = document.querySelector(".meme-bottom-text");
 const memeBottomTextEditor = document.querySelector("#bottom-text-editor");
 const bottomTextCheckbox = document.querySelector("#no-bottom-text");
+const fontSelection = document.querySelector("#font-selection");
+const arialFont = document.querySelector("#arial");
+const arialBlackFont = document.querySelector("#arial-black");
+const americanTypewriterFont = document.querySelector("#american-typewriter");
+const comicSansFont = document.querySelector("#comic-sans");
+const helveticaFont = document.querySelector("#helvetica");
+const impactFont = document.querySelector("#impact");
+const verdanaFont = document.querySelector("#verdana");
+const timesNewRomanFont = document.querySelector("#times-new-roman");
 
 // Boton para que se vea el aside de texto
 
@@ -19,7 +28,7 @@ memeTopTextEditor.oninput = () => {
     memeTopText.textContent = memeTopTextEditor.value;
 }
 
-// Checkbox para eliminar el texto superior del mem
+// Checkbox para eliminar el texto superior del meme
 
 topTextCheckbox.onclick = () => {
     memeTopText.classList.toggle("hide");
@@ -35,4 +44,41 @@ memeBottomTextEditor.oninput = () => {
 
 bottomTextCheckbox.onclick = () => {
     memeBottomText.classList.toggle("hide");
+}
+
+// Selector de tipografía del meme
+
+fontSelection.onchange = () => {
+    if (fontSelection.value === "arial") {
+        memeTopText.style.fontFamily = "Arial";
+        memeBottomText.style.fontFamily = "Arial";
+    }
+    else if (fontSelection.value === "arial-black") {
+        memeTopText.style.fontFamily = "Arial Black";
+        memeBottomText.style.fontFamily = "Arial Black";
+    }
+    else if (fontSelection.value === "american-typewriter") {
+        memeTopText.style.fontFamily = "American Typewriter";
+        memeBottomText.style.fontFamily = "American Typewriter";
+    }
+    else if (fontSelection.value === "comic-sans") {
+        memeTopText.style.fontFamily = "Comic Sans MS";
+        memeBottomText.style.fontFamily = "Comic Sans MS";
+    }
+    else if (fontSelection.value === "helvetica") {
+        memeTopText.style.fontFamily = "Helvetica";
+        memeBottomText.style.fontFamily = "Helvetica";
+    }
+    else if (fontSelection.value === "impact") {
+        memeTopText.style.fontFamily = "Impact";
+        memeBottomText.style.fontFamily = "Impact";
+    }
+    else if (fontSelection.value === "verdana") {
+        memeTopText.style.fontFamily = "Verdana";
+        memeBottomText.style.fontFamily = "Verdana";
+    }
+    else if (fontSelection.value === "times-new-roman") {
+        memeTopText.style.fontFamily = "Times New Roman";
+        memeBottomText.style.fontFamily = "Times New Roman";
+    }
 }
